6720a24d2201eb5610d23d76ee248f9734d3e58d
Patrycja <patrycjabezrak@gmail.com>
2023-10-23 19:37:24 +0200

    odejmowanie

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,9 +5,21 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("siema elo");
         Console.WriteLine(Dodawanie());
+        Console.WriteLine(Odejmowanie());
+        
     }
     private static byte Dodawanie()
     {
         return 2 + 3;
+    }
+
+    private static int Odejmowanie()
+    {
+        return 2 - 3;
+    }
+
+    private static byte Mnozenie()
+    {
+        return 2 * 3;
     }
 }
